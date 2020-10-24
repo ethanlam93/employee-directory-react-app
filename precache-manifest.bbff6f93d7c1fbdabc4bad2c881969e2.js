75d372afa83bc4acf8bfaffdd0bd1ea11fcae2e4
Ethan Lam <ethanlam93@gmail.com>
2020-10-24 19:38:02 -0400

    Updates

---
+++ precache-manifest.bbff6f93d7c1fbdabc4bad2c881969e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d360441eff73d82d041adbf93991e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1037724e6b62d631b5db",
+    "url": "/static/js/2.2b999cac.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/static/js/2.2b999cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a2d7c52684de0e32ac",
+    "url": "/static/js/main.a2a74b0d.chunk.js"
+  },
+  {
+    "revision": "0239a00348e1c49388c7",
+    "url": "/static/js/runtime-main.898530a7.js"
+  }
+]);
